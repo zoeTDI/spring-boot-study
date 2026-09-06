6d3b8fa9f065f92e0c98b9179d1f54412ea7f166
Caldm <1832400547@qq.com>
2026-09-06 21:16:30 +0800

    build(project): 初始化仓库

---
+++ src/main/java/cn/study/caldm/CaldmApplication.java
@@ -0,0 +1,13 @@
+package cn.study.caldm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaldmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaldmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/study/caldm/CaldmApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.study.caldm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaldmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
