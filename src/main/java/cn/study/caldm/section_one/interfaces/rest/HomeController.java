6d293943238943e4eb3b72d65e52182e509e243b
Caldm <1832400547@qq.com>
2026-09-07 22:27:51 +0800

    feat: properties

--- src/main/java/cn/study/caldm/section_one/interfaces/rest/HomeController.java
+++ src/main/java/cn/study/caldm/section_one/interfaces/rest/HomeController.java
@@ -1,15 +1,20 @@
 package cn.study.caldm.section_one.interfaces.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
+    @Value("${spring.application.name}")
+    private String appName;
+
     @RequestMapping("/")
     public String index() {
         String viewname = getViewName();
         System.out.println(viewname);
+        System.out.println(appName);
         return viewname;
     }
 
@@ -18,4 +23,3 @@ public class HomeController {
     }
 
 }
- 
