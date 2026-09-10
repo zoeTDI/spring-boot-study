9eb2e218c865c20477fc72d7990b2b60679bd315
Caldm <1832400547@qq.com>
2026-09-10 23:03:07 +0800

    序列化控制

--- src/main/java/cn/study/caldm/section_two/infrastructure/persistence/mapper/UserMapper.java
+++ src/main/java/cn/study/caldm/section_two/infrastructure/persistence/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package cn.study.caldm.section_two.infrastructure.persistence.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import cn.study.caldm.section_two.domain.model.User;
 import cn.study.caldm.section_two.interfaces.dtos.UserDto;
@@ -8,5 +9,6 @@ import cn.study.caldm.section_two.interfaces.dtos.UserDto;
 
 @Mapper (componentModel = "spring")
 public interface UserMapper {
+    // @Mapping (target = "createAt", expression = "java(java.time.LocalDateTime.now())")
     UserDto toDto(User user);
 }
--- src/main/java/cn/study/caldm/section_two/interfaces/dtos/UserDto.java
+++ src/main/java/cn/study/caldm/section_two/interfaces/dtos/UserDto.java
@@ -1,7 +1,13 @@
 package cn.study.caldm.section_two.interfaces.dtos;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import cn.study.caldm.section_two.domain.model.User;
 import lombok.AllArgsConstructor;
@@ -10,7 +16,13 @@ import lombok.Data;
 @AllArgsConstructor
 @Data
 public class UserDto {
+    // @JsonIgnore // 忽略
+    @JsonProperty ("user_id") // 重命名
     private Long id;
     private String name;
     private String email;
+    // @JsonInclude (JsonInclude.Include.NON_NULL) // 排除 null
+    // private String phoneNumber;
+    // @JsonFormat (pattern = "yyyy-MM-dd HH:mm:ss") // 格式化
+    // private LocalDateTime createAt;
 }
